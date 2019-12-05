bde75f3862e8c56ab0f83b749c14ee64aff0e7bf
karim2989 <karim2989@gmail.com>
2019-12-05 19:32:52 +0100

    remove comments

--- Startup.cs
+++ Startup.cs
@@ -36,58 +36,16 @@ namespace ConvertItOnline
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                /*endpoints.MapGet("/", async context =>
                  {
-                     await context.Response.WriteAsync(@"Loading ...");
                      context.Response.Redirect("/index.html", true);
-                 });
-                endpoints.MapGet("/text", async context =>
-                {
-                    await context.Response.WriteAsync(File.ReadAllText("./text.txt"));
-                });
-                //endpoints.MapRazorPages();*/
+                 });*/
                 endpoints.MapPost("/Process", async context =>
                 {
                     await context.Response.WriteAsync("");
                     ProcessController.AcceptRequest(context);
                 });
-                /*async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                    var req = context.Request;
-                    string formItems = "";
-                    foreach (var key in req.Form.Keys)
-                    {
-                        formItems += key + req.Form[key];
-                    }
-                    string formfiles = "";
-                    foreach (var file in req.Form.Files)
-                    {
-                        formfiles += file.Name + file.Length;
-                    }
-                    File.WriteAllText("headers.bin", formItems);
-                    File.WriteAllText("files.bin", formfiles);
-                    //File.WriteAllText("headers.bin", req.Form.Files.GetFile(""));
-                    File.WriteAllBytes("input.bin", ReadStream(req.Body));
-                    readImage(req.Form.Files[0]).Save("input.png");
-                    /*var reader = new StreamReader(context.Request.Body);
-                    System.Console.WriteLine(reader.ReadToEnd());
-                });*/
             });
         }
-        private byte[] ReadStream(Stream s)
-        {
-            var reader = new StreamReader(s);
-            string body = reader.ReadToEndAsync().Result;
-            /*while (!reader.EndOfStream)
-            {
-                output+=reader.readd
-            }*/
-            var output = new byte[body.Length];
-            for (int i = 0; i < body.Length; i++)
-            { output[i] = (byte)body[i]; }
-            return output;
-        }
-        private Image readImage(IFormFile file) => Bitmap.FromStream(file.OpenReadStream());
     }
 }
